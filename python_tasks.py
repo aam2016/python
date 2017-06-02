e86d5fc038f3288c6d7150d9b90785914321e680
Alexander <alexander.ananiev@gmail.com>
2017-06-02 16:25:10 +0300

    Update python_tasks.py

--- python_tasks.py
+++ python_tasks.py
@@ -12,3 +12,24 @@ def solution(A):
 # (Бинарные четные числа оканчиваются на 0, нечетные – на 1)
 def is_even(n):
     return True if bin(n)[-1] == "0" else False
+
+
+# Build Tower by the following given argument:
+# number of floors (integer and always greater than 0).
+# for example, a tower of 3 floors looks like below
+# [
+#   '  *  ', 
+#   ' *** ', 
+#   '*****'
+# ]
+def tower_builder(n_floors):
+    tower = []
+    i = 1
+    j = 1
+    n = n_floors
+    while i <= n_floors:
+        tower.append(" " * (n - 1) + "*" * j + " " * (n - 1))
+        i += 1
+        j += 2
+        n -= 1
+    return tower
